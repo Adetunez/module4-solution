4f9a09a53fe28afa6d2c885de427daf8d1396a11
Itunu <adeyemoitunu1@gmail.com>
2022-04-29 16:04:30 +0100

    page

---
+++ site/script.js
@@ -0,0 +1,11 @@
+var names = ["joke", "timi", "Jade"];
+console.log(names);
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello " + names[i]);
+}
+if (names = "j/J"){
+    console.log("Goodbye JSomeName");
+}
+else{
+    console.log("Hello SomeName");
+}
